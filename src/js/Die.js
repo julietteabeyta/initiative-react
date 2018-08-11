9bfc199ae815bf2464378b1ae38679d4f4748b61
julietteabeyta <juneauwh0a@gmail.com>
2018-08-11 16:01:21 -0600

    Add dynamic dice rolling WIP::Styling

--- src/js/Die.js
+++ src/js/Die.js
@@ -1,8 +1,29 @@
 import React, {Component} from 'react';
 
 class Die extends Component {
+  state = {
+    rollValue: this.props.sides,
+    rolling: false
+  }
+  roll = e => {
+      e.preventDefault();
+
+      const sides = this.props.sides;
+      this.setState({ rollValue: Math.ceil(Math.random() * sides), rolling: true });
+  }
+
   render() {
-    return("hello");
+    const sides = this.props.sides;
+    const sidesString = `d${sides}`;
+    const isD4 = sides === 4;
+    return (
+        <div class="die-parent">
+          <div>{sides}</div>
+          <div id={sidesString} className={this.state.rolling ? 'rolling die' : 'die'} data-d4={isD4 ? this.state.rollValue : null} onClick={this.roll}>
+              <div className="readout">{this.state.rollValue}</div>
+          </div>
+        </div>
+      );
   }
 }
 
